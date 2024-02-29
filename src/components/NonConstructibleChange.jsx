0f08b592ede676d4a2796c5a81d99e8e173a7ef2
braiaguirre <brianaguirre.sp94@gmail.com>
2024-02-29 15:26:32 -0300

    nonconstructiblechange done

--- src/components/NonConstructibleChange.jsx
+++ src/components/NonConstructibleChange.jsx
@@ -1,12 +1,37 @@
+import cleanArray from '../utils/cleanArray';
+import nonConstructibleChange from '../../algorithms/nonConstructibleChange';
+
+import { useRef, useState } from 'react';
+
 import styles from './NonConstructibleChange.module.css';
 
 const NonConstructibleChange = () => {
+  const arrayRef = useRef();
+  const resultRef = useRef();
+  let [array, setArray] = useState([]);
+  
+  const handleSetArray = (e) => setArray(cleanArray(e.target.value));
+  const handleCalculate = () => resultRef.current.value = nonConstructibleChange(array);
+  const handleClear = () => {
+    arrayRef.current.value = '';
+    resultRef.current.value = '';
+  }
+
   return (
     <>
       <div className={ styles.container }>
-        <h3>Non-Constructible Change</h3>
+        <h3>Two Number Sum</h3>
         <p>Given an array of positive integers representing the values of coins in your possession, write a function that returns the minimum amount of change (the minimum sum of money) that you cannot create. The given coins can have any positive integer value and aren’t necessarily unique (i.e., you can have multiple coins of the same value).</p>
-        
+
+        <h4>Array of positive integers, separated by any character</h4>
+        <input type="text" ref={ arrayRef } onChange={ handleSetArray } />
+        <h4>Result</h4>
+        <input type="text" ref={ resultRef } disabled />
+        <div>
+          <button onClick={ handleCalculate }>Calculate</button>
+          <button onClick={ handleClear }>Clear</button>
+        </div>
+
       </div>
     </>
   )
--- src/components/TwoNumberSum.jsx
+++ src/components/TwoNumberSum.jsx
@@ -6,6 +6,8 @@ import { useRef, useState } from 'react';
 import styles from './TwoNumberSum.module.css';
 
 const TwoNumberSum = () => {
+  const arrayRef = useRef();
+  const targetSumRef = useRef();
   const resultRef = useRef();
   let [array, setArray] = useState([]);
   let [targetSum, setTargetSum] = useState(0);
@@ -13,7 +15,11 @@ const TwoNumberSum = () => {
   const handleSetArray = (e) => setArray(cleanArray(e.target.value));
   const handleSetTargetSum = (e) => setTargetSum(e.target.value);
   const handleCalculate = () => resultRef.current.value = `[${twoNumberSum(array, targetSum)}]`;
-  const handleClear = () => resultRef.current.value = '';
+  const handleClear = () => {
+    arrayRef.current.value = '';
+    targetSumRef.current.value = '';
+    resultRef.current.value = '';
+  }
 
   return (
     <>
@@ -21,16 +27,16 @@ const TwoNumberSum = () => {
         <h3>Two Number Sum</h3>
         <p>Given an array of integers, no number in this array is repeated, and an integer representing the target sum, implement a function that find whether there's a pair of numbers in the array that adds up to the target sum. Return the pair in an array. If such pair does not exist, return an empty array.</p>
 
-        <h4>Array of integers, separated by any character:</h4>
-        <input type="text" onChange={ handleSetArray } />
-        <h4>Target sum:</h4>
-        <input type="number" onChange={ handleSetTargetSum } />
+        <h4>Array of integers, separated by any character</h4>
+        <input type="text" ref={ arrayRef } onChange={ handleSetArray } />
+        <h4>Target sum</h4>
+        <input type="number" ref={ targetSumRef } onChange={ handleSetTargetSum } />
+        <h4>Result</h4>
+        <input type="text" ref={ resultRef } disabled />
         <div>
           <button onClick={ handleCalculate }>Calculate</button>
           <button onClick={ handleClear }>Clear</button>
         </div>
-        <h4>Result:</h4>
-        <input type="text" ref={ resultRef } disabled />
 
       </div>
     </>
